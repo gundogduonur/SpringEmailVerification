da2b2009ffbaa6c79e60d9c0a693091343111f25
Onur Gündoğdu <gundogdusoftware@outlook.com>
2022-02-17 00:46:06 +0300

    Security

--- src/main/java/com/onurgundogdu/emailverify/service/RegistrationService.java
+++ src/main/java/com/onurgundogdu/emailverify/service/RegistrationService.java
@@ -4,6 +4,6 @@ import com.onurgundogdu.emailverify.request.RegistrationRequest;
 
 public class RegistrationService {
     public static String register(RegistrationRequest request) {
-        return null;
+        return "works";
     }
 }
